cc8f99be2660a3edfa3ffdce2f9e5020601b5e24
Tomoro Warashina <tw@TomoronoMacBook-Pro.local>
2020-02-09 11:31:10 +0900

    2020-02-09_11:31:10

---
+++ scripts/7_ls-bsr.sh
@@ -0,0 +1,21 @@
+#PBS -q small
+#PBS -l ncpus=40
+#PBS -V
+cd ${PBS_O_WORKDIR}
+source activate ls_bsr
+
+mkdir -p analysis/ls-bsr && cd $_
+mkdir {data,db,query}
+
+# make database (fna)
+cp ../../data/dataset/*.gbff ./data
+for f in ./data/*.gbff; do python ../../scripts/gbk2fna.py $f; done
+mv data/*.fasta db
+
+# make query (faa)
+for f in ./data/*.gbff; do python ../../scripts/gbk2faa.py $f > $f.faa; done
+cat data/*.faa > data/all.faa
+mv data/all.faa query
+
+# run LS-BSR
+python /home/t16965tw/downloads/LS-BSR/ls_bsr.py -p 40 -d db -g query/all.faa
---
+++ scripts/ex_16SrRNA 2.py
@@ -0,0 +1,20 @@
+from Bio import SeqIO, SeqFeature
+import sys
+
+FNAME = sys.argv[1]
+
+gbank=SeqIO.parse(open(FNAME,"rU"),"genbank")
+
+for genome in gbank:
+    parental_seq = genome.seq
+    i=0
+    for gene in genome.features:
+        if gene.type=="rRNA":
+            if 'product' in gene.qualifiers:
+                if '16S' in gene.qualifiers['product'][0]:
+                    seq_slice = gene.location.extract(parental_seq)
+                    i = i+1
+                    if 'db_xref' in gene.qualifiers:
+                        print(">%s_%s\n%s" % (FNAME,i,seq_slice))
+                    else:
+                        print(">%s_%s\n%s" % (FNAME,i,seq_slice))
